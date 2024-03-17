2a0a1372fb9ee3f119e9098a60020e711f2bee53
Deepanshu Kumawat <161687415+deepanshukmwt@users.noreply.github.com>
2024-03-17 12:14:54 +0000

    variables and datatypes

---
+++ 01_basics.js/02_datatypes.js
@@ -0,0 +1,35 @@
+/* three types of varibles:
+use: hum user se koi value lete hain for exam date name etc, kahin toh usse store krenge toh value store
+krne ke liye variable use krte hai. its just a space in memory which store values.
+
+three types.
+1) var 2)let 3) const
+const: clear from name that its value can be change.
+let var: use use to prefer let because it can fix the issue of block and funxtion scope.
+
+
+datatypes:
+1)number
+2)boolean 
+3)string
+4)bigint
+5)null
+6)undefined
+7)symbol (for uniquness)
+
+*/
+
+let name = "deepanshu"
+var classN = "collge"
+const password = 2315911
+
+console.log([name , classN , password]);
+
+name = "sourav";
+classN= 12;
+// const password = if i change it it will give error
+console.log([name , classN , typeof classN]); // typeof use to find the datatype of variable
+
+
+
+
